6305b8a871a3f3b9d9b2c5c7841cdc715c6d2417
FanonX Rogers <fanonxdevelops@gmail.com>
2020-03-17 01:58:40 -0400

    Added correct recover link

--- src/main/java/com/fanonx/coronavirustracker/constants/ConstantsUtil.java
+++ src/main/java/com/fanonx/coronavirustracker/constants/ConstantsUtil.java
@@ -4,5 +4,5 @@ public class ConstantsUtil {
   // constants throughout the application
   public static String VIRUS_CONFIRMED_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
   public static String VIRUS_DEATH_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
-  public static String VIRUS_RECOVERED_DATA_URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
+  public static String VIRUS_RECOVERED_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
 }
