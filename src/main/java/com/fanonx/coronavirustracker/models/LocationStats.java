5f6c487223a7abfa8a3671d86c17b25a8077ee1e
FanonX Rogers <fanonxdevelops@gmail.com>
2020-03-16 23:53:49 -0400

    Models created.

--- src/main/java/com/fanonx/coronavirustracker/models/LocationStats.java
+++ src/main/java/com/fanonx/coronavirustracker/models/LocationStats.java
@@ -1,5 +1,50 @@
 package com.fanonx.coronavirustracker.models;
 
 public class LocationStats {
+  /** Location model to show corona virus statistics in each state*/
+  private String state;
+  private String country;
+  private int latestTotalCases;
+  private int diffFromPreviousDay;
 
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public int getLatestTotalCases() {
+    return latestTotalCases;
+  }
+
+  public void setLatestTotalCases(int latestTotalCases) {
+    this.latestTotalCases = latestTotalCases;
+  }
+
+  public int getDiffFromPreviousDay() {
+    return diffFromPreviousDay;
+  }
+
+  public void setDiffFromPreviousDay(int diffFromPreviousDay) {
+    this.diffFromPreviousDay = diffFromPreviousDay;
+  }
+
+  @Override
+  public String toString() {
+    return "LocationStats{" +
+        "state='" + state + '\'' +
+        ", country='" + country + '\'' +
+        ", latestTotalCases=" + latestTotalCases +
+        '}';
+  }
 }
