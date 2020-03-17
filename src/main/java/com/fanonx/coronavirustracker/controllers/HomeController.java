8c540b379dbada5bac314152ca13cdaa302318b8
FanonX Rogers <fanonxdevelops@gmail.com>
2020-03-16 23:53:30 -0400

    Controller for sending models to the views.

--- src/main/java/com/fanonx/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/fanonx/coronavirustracker/controllers/HomeController.java
@@ -1,5 +1,27 @@
 package com.fanonx.coronavirustracker.controllers;
 
-public class HomeController {
+import com.fanonx.coronavirustracker.models.LocationStats;
+import com.fanonx.coronavirustracker.service.CoronaVirusDataService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+  /** Controller class to generate/render the html UI */
+  @Autowired
+  CoronaVirusDataService coronaVirusDataService;
+
+  @GetMapping("/") // map this to the root template
+  public String home(Model model) {
+    List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+    int totalConfirmedCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+    int totalNewCases = allStats.stream().mapToInt(LocationStats::getDiffFromPreviousDay).sum();
+    model.addAttribute("locationStats", allStats);
+    model.addAttribute("totalReportedCases", totalConfirmedCases);
+    model.addAttribute("totalNewCases", totalNewCases);
+    return "home";
+  }
 }
